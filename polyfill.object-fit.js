8d3699547e83b8f0304f29ed170b2b63387da42f
Christian Schepp Schaefer <schaepp@gmx.de>
2013-11-22 22:14:18 +0100

    - Reworked the whole polyfill
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

--- polyfill.object-fit.js
+++ polyfill.object-fit.js
@@ -13,91 +13,202 @@
 
 	// Storage variable
 	var objectFit = {};
+	
+	objectFit._debug = true;
 
-	// Detects Img Orientation
-	objectFit.orientation = function(object) {
-		var imgOrientation;
-		var imgWidth = parseInt(window.getComputedStyle(object).width, 10);
-		var imgHeight = parseInt(window.getComputedStyle(object).height, 10);
+	objectFit.getComputedStyle = function(element, context) {
+		var context = context || window;
 
-		if (imgWidth > imgHeight) {
-			imgOrientation = 'landscape';
+		if (context.getComputedStyle) {
+			return context.getComputedStyle(element, null);
 		}
-		else if (imgWidth < imgHeight) {
-			imgOrientation = 'portrait';
+		else {
+			return element.currentStyle;
 		}
-		else if (imgWidth = imgHeight) {
-			imgOrientation = 'square';
-		}
+	}
 
-		return imgOrientation;
+	objectFit.getDefaultComputedStyle = function(element){
+		var newelement = element.cloneNode(true);
+		var styles = {}
+		var iframe = document.createElement('iframe');
+		document.body.appendChild(iframe);
+		iframe.contentWindow.document.open();
+		iframe.contentWindow.document.write('<body></body>');
+		iframe.contentWindow.document.body.appendChild(newelement);
+		iframe.contentWindow.document.close();
+		
+		var defaultElement = iframe.contentWindow.document.querySelectorAll(element.nodeName.toLowerCase())[0];
+		var defaultComputedStyle = this.getComputedStyle(defaultElement, iframe.contentWindow);
+		
+		for (var property in defaultComputedStyle) {
+			var value = defaultComputedStyle.getPropertyValue(property);
+			if (value !== null) {
+				switch (property) {
+					default:
+						styles[property] = value;
+					break;
+					
+					case 'width':
+					case 'height':
+					case 'minWidth':
+					case 'minHeight':
+					case 'maxWidth':
+					case 'maxHeight':
+					break;
+				}
+			}
+		}
+		
+		document.body.removeChild(iframe);
+
+		return styles;
+	}
+	
+	objectFit.getMatchedStyle = function(element, property){
+		// element property has highest priority
+		var val = element.style.getPropertyValue(property);
+	
+		// if it's important, we are done
+		if (val !== null) return val;
+	
+		// get matched rules
+		var rules = getMatchedCSSRules(element);
+	
+		// iterate the rules backwards
+		// rules are ordered by priority, highest last
+		for (var i = rules.length; i --> 0;){
+			var r = rules[i];
+	
+			var important = r.style.getPropertyPriority(property);
+	
+			// if set, only reset if important
+			if (val === null || important) {
+				val = r.style.getPropertyValue(property);
+	
+				// done if important
+				if (important) break;
+			}
+		}
+	
+		return val;
+	}
+	
+	// Detects orientation
+	objectFit.orientation = function(replacedElement) {
+		if (replacedElement.parentNode.nodeName.toLowerCase() === 'x-object-fit') {
+			var width = replacedElement.naturalWidth || replacedElement.clientWidth;
+			var height = replacedElement.naturalHeight || replacedElement.clientHeight;
+			var parentWidth = replacedElement.parentNode.clientWidth;
+			var parentHeight = replacedElement.parentNode.clientHeight;
+	
+			if (!height || width / height > parentWidth / parentHeight) {
+				replacedElement.className = ' x-object-fit-wider';
+				if (this._debug && window.console) console.log('x-object-fit-wider');
+			} else {
+				replacedElement.className = 'x-object-fit-taller';
+				if (this._debug && window.console) console.log('x-object-fit-taller');
+			}
+		}
 	}
 
 	// Contains the real polyfill
-	objectFit.polyfill = function(object, type) {
-
-		// Define ultimate fallback for type
-		if (type === undefined) {
-			var type = 'cover';
-		}
-
-		var objectSelector = document.querySelectorAll(object);
-		var selectorByType;
-
-		if (document.getElementsByClassName('object-fit-cover').length > 0) {
-			type = 'cover';
-			selectorByType = document.getElementsByClassName('object-fit-cover');
-		}
-		else if (document.getElementsByClassName('object-fit-contain').length > 0) {
-			type = 'contain';
-			selectorByType = document.getElementsByClassName('object-fit-contain');
-		}
-		else {
-			type = 'cover';
-			selectorByType = objectSelector;
-		}
-
-		// Wrap elements, then apply effect through CSS classes
-		if (type==='cover') {
-			// Wrap img elements with surrounding div
-			for (var i=0; i < selectorByType.length; i++) {
-				var img = selectorByType[i];
-				var parent = img.parentNode;
-
-				var imgOrientation = objectFit.orientation(img);
-
-				var wrapper = document.createElement('div');
-
-				var wrapperCSSClasses = 'object-fit object-fit-cover';
-				var imgCSSClasses = imgOrientation;
-
-				wrapper.setAttribute('class', wrapperCSSClasses);
-				img.className += imgCSSClasses;
-
-				parent.insertBefore(wrapper, img);
-				wrapper.appendChild(img);
-
+	objectFit.process = function(args) {
+		if(!args.selector) return;
+		args.fittype = args.fittype || 'none';
+		switch (args.fittype) {
+			default:
 				return;
-			}
+			break;
+			
+			case 'none':
+			case 'fill':
+			case 'contain':
+			case 'cover':
+			break;
 		}
-		else if (type==='contain') {
-			console.log('Class found: object-fit-contain');
-
-			// Wrap img elements with surrounding div
-			for (var i = 0; i < selectorByType.length; i++) {
-				var img = selectorByType[i];
-				var parent = img.parentNode;
-
-				var wrapper = document.createElement('div');
-				wrapper.setAttribute('class','object-fit object-fit-contain');
-
-				parent.insertBefore(wrapper, img);
-				wrapper.appendChild(img);
-
-				return;
-			}
+		
+		var replacedElements = document.querySelectorAll(args.selector);
+		if(!replacedElements.length) return;
+		
+		for (var i = 0, replacedElements_length = replacedElements.length; i < replacedElements_length; i++) {
+			(function(){
+				var replacedElement = replacedElements[i];
+				
+				var replacedElementStyles = objectFit.getComputedStyle(replacedElement);
+				var replacedElementDefaultStyles = objectFit.getDefaultComputedStyle(replacedElement);
+				
+				var wrapperElement = document.createElement('x-object-fit');
+				
+				if (objectFit._debug && window.console) console.log('Applying to WRAPPER-------------------------------------------------------');
+				for (var property in replacedElementStyles) {
+					var value = objectFit.getMatchedStyle(replacedElement,property); //replacedElementStyles.getPropertyValue(property);
+					if (value !== null) {
+						if (objectFit._debug && window.console) console.log(property + ': ' + value);
+						wrapperElement.style[property] = value;
+					}
+				}
+	
+				if (objectFit._debug && window.console) console.log('Applying to REPLACED ELEMENT-------------------------------------------------------');
+				for (var property in replacedElementDefaultStyles) {
+					var value = replacedElementDefaultStyles[property];
+					if (objectFit._debug && window.console) console.log(property + ': ' + value);
+					replacedElement.style[property] = value;
+				}
+	
+				wrapperElement.setAttribute('class','x-object-fit-' + args.fittype);
+				replacedElement.parentNode.insertBefore(wrapperElement, replacedElement);
+				wrapperElement.appendChild(replacedElement);
+				
+				objectFit.orientation(replacedElement);
+	
+				var resizeTimer = null;
+				var resizeAction = (function(){
+					if(resizeTimer !== null) window.clearTimeout(resizeTimer);
+					resizeTimer = window.setTimeout(function(){
+						objectFit.orientation(replacedElement);
+					},50);
+				});
+				
+				switch (args.fittype) {
+					default:
+					break;
+					
+					case 'contain':
+					case 'cover':
+						if (window.addEventListener) {
+							window.addEventListener('resize', resizeAction, false);
+							window.addEventListener('orientationchange', resizeAction, false);
+						} else {
+							window.attachEvent('onresize', resizeAction);
+						}
+					break;
+				}
+			})();
 		}
 	};
+	
+	objectFit.init = function(args) {
+		if(!args) return;
+
+		if (typeof args === 'Array') {
+			for (var i = 0, args_length = args.length; i < args_length; i++) {
+				this.process(args[i]);
+			}
+		}
+		else {
+			this.process(args);
+		}
+	}
+	
+	objectFit.polyfill = function(args) {
+		if('objectFit' in document.documentElement.style === false) {
+			if (window.addEventListener) {
+				window.addEventListener('load', function(){objectFit.init(args)}, false);
+			} else {
+				window.attachEvent('onload', function(){objectFit.init(args)});
+			}
+		}
+	}
 
 	// Export into global space
 	global.objectFit = objectFit;
