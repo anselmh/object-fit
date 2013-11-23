0fa41a1eaef8e2d70494dbc0b1733088afcf4ca1
Anselm Hannemann <info@anselm-hannemann.com>
2013-11-23 18:04:21 +0100

    clean up repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	//grunt.loadNpmTasks('grunt-contrib-connect');
 
 	// A task for development
 	grunt.registerTask('dev', ['jshint']);
